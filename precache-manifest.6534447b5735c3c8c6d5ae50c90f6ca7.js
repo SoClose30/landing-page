10c64c3ce21ce7d0decd5b4bb9075a989ecef0a4
SoClose30 <emmanuelsalvocv@gmail.com>
2022-03-01 15:46:34 -0300

    Updates

---
+++ precache-manifest.6534447b5735c3c8c6d5ae50c90f6ca7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "1006ac7d1e7f5ae7dba2",
+    "url": "/landing-page/static/css/main.40347332.chunk.css"
+  },
+  {
+    "revision": "1006ac7d1e7f5ae7dba2",
+    "url": "/landing-page/static/js/main.1006ac7d.chunk.js"
+  },
+  {
+    "revision": "067c18fc1b316baba1a6",
+    "url": "/landing-page/static/js/1.067c18fc.chunk.js"
+  },
+  {
+    "revision": "5e502b1b7d9d8dee4025",
+    "url": "/landing-page/static/js/runtime~main.5e502b1b.js"
+  },
+  {
+    "revision": "dcb00daf8e892c2fc9f74ecd241e6a86",
+    "url": "/landing-page/static/media/rocket.dcb00daf.png"
+  },
+  {
+    "revision": "95d9a4050977b8a702750d6cac93dfec",
+    "url": "/landing-page/static/media/shield.95d9a405.png"
+  },
+  {
+    "revision": "7519796373db9c05abaa66cca63f0416",
+    "url": "/landing-page/static/media/wallet.75197963.png"
+  },
+  {
+    "revision": "9c194cafb04003bbe938d071890b5445",
+    "url": "/landing-page/static/media/gplogo.9c194caf.svg"
+  },
+  {
+    "revision": "4d995515c1cf81085df61c9d4ed0f5ca",
+    "url": "/landing-page/static/media/apple.4d995515.svg"
+  },
+  {
+    "revision": "ea4e1d80a509fa3d9a377b1a916d5989",
+    "url": "/landing-page/static/media/windows.ea4e1d80.svg"
+  },
+  {
+    "revision": "85088f8fc89c324fe44979d59cc73f7c",
+    "url": "/landing-page/static/media/card-front-color.85088f8f.png"
+  },
+  {
+    "revision": "c0569d58eafc7be238c4ad41d7175f61",
+    "url": "/landing-page/index.html"
+  }
+];
